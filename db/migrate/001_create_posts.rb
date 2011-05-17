9ab1319eb17a6ad9bf7684c11c44852777ac5ae7
Samir Zeka <samirzeka@rodi.nl>
2011-05-17 10:08:08 +0200

    changing migrations to work with postgresql

--- db/migrate/001_create_posts.rb
+++ db/migrate/001_create_posts.rb
@@ -2,14 +2,15 @@ class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
       t.column :title,      :string, :limit => 100, :default => "", :null => false
-      t.column :body,       :text,                  :default => "", :null => false
-      t.column :author,     :string, :limit => 100, :default => 0,  :null => false
+      t.column :content,    :text,                  :default => "", :null => false
+      t.column :author_id,  :integer,               :default => 0,  :null => false
       t.column :category,   :string, :limit => 20,  :default => "", :null => false
       t.column :status,     :string, :limit => 20,  :default => "", :null => false
       t.column :created_at, :datetime
-
-
+      t.column :updated_at, :datetime
     end
+
+    add_index :posts, :author_id
   end
 
   def self.down
